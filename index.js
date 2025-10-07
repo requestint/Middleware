4b48c6776eb339a862df9c050e6a8398f5969b0b
studio <studiowork1020@gmail.com>
2025-10-07 18:41:49 -0400

    update

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ app.use(express.json());
 
 let commandQueue = [];
 
+
 app.post('/send-command', (req, res) => {
-  const { type, username } = req.body;
+  const { type, username, length } = req.body;
   
   if (!type || !username) return res.status(400).send('Missing fields');
-  commandQueue.push({ type, username });
+  commandQueue.push({ type, username, length });
+
+  console.log(`Port ${3000} Remote Session Request : ` + JSON.stringify(req.body))
   res.sendStatus(200);
 });
 
 app.get('/get-commands', (req, res) => {
+
+
+  console.log(`Port ${3000} Remote Session Request : ` + JSON.stringify(req.body))
+
   const commands = [...commandQueue];
   commandQueue = [];
   res.json(commands);
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
